424bf298eb094267b149eb2e3dd3d2c8b63a1bf3
ejoejo <ejoejo@gmail.com>
2017-07-25 14:42:33 +0800

    renew

--- homework2.rb
+++ homework2.rb
@@ -26,25 +26,24 @@ class RPS
     puts "|===============================================|"
 
     puts "請輸入您的名字："
-    @name = gets.chomp.upcase
+    @name=gets.chomp.upcase
   end
 
   def decide
     #邏輯判斷式
     if @user_input == @computer_input
-      puts "平手"
+        puts "平手"
     else
-      case @user_input 
-      when "S" then puts (@computer_input == "R") ? + @name + " 出剪刀(S)，電腦出石頭(R)，電腦贏！" : + @name + " 出剪刀(S)，電腦出布(P)， " + @name + " 贏！"
-      when "R" then puts (@computer_input == "P") ? + @name + " 出石頭(R)，電腦出布(P)，電腦贏！" : + @name + " 出石頭(R)，電腦出剪刀(S)， " + @name + " 贏！" 
-      when "P" then puts (@computer_input == "S") ? + @name + " 出布(P)，電腦出剪刀(S)，電腦贏！" : + @name + " 出布(P)，電腦出石頭(R)， " + @name + " 贏！"
-      end
-    end
-  end
+        case @user_input 
+        when "S" then puts (@computer_input == "R") ? @name + " 出剪刀(S)，電腦出石頭(R)，電腦贏！" :  @name + " 出剪刀(S)，電腦出布(P)， " + @name + " 贏！"
+        when "R" then puts (@computer_input == "P") ? @name + " 出石頭(R)，電腦出布(P)，電腦贏！" :  @name + " 出石頭(R)，電腦出剪刀(S)， " + @name + " 贏！" 
+        when "P" then puts (@computer_input == "S") ? @name + " 出布(P)，電腦出剪刀(S)，電腦贏！" :  @name + " 出布(P)，電腦出石頭(R)， " + @name + " 贏！"
+        end
+    end  end
 
   def get_player_gestures
     #取得玩家和電腦兩個物件的
-    puts @name + " 的選擇：剪刀(S)、石頭(R)、布(P)"
+    puts @name+" 的選擇：剪刀(S)、石頭(R)、布(P)"
     human = Human.new(@name)
     @user_input = human.get_gesture
     computer = Computer.new(computer)
@@ -54,8 +53,8 @@ class RPS
   def continue?
     #判斷玩家是否要繼續下一輪
     begin
-      puts "繼續玩?: 是(Y) / 否(N)"
-      continue = gets.chomp.upcase
+        puts "繼續玩?: 是(Y) / 否(N)"
+        continue = gets.chomp.upcase
     end while !["Y", "N"].include?(continue)
     continue
   end
@@ -68,10 +67,10 @@ class RPS
   def initialize
     intro
     begin
-      puts ""
-      get_player_gestures
-      decide
-      continue = continue?
+        puts ""
+        get_player_gestures
+        decide
+        continue = continue?
     end while continue == "Y"
     show_message
   end
